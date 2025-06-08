56285a2cec9d1d4834017c78c494062a02981f8d
Samara Chaves <samarachaves1999@hotmail.com>
2025-06-08 19:35:47 -0300

    Update SuperTrunfo2.c
    Inseri os cálculos de densidade populacional e PIB per capita

--- SuperTrunfo2.c
+++ SuperTrunfo2.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main() {
-char codigo1[4] = "A1", nome1[50] = "Santos";
-char codigo2[4] = "B2", nome2[50] = "Londrina";
-int populacao1 = 418608, populacao2 = 577318;
-int pontos1 = 10, pontos2 = 80;
-float area1 = 280674, area2 = 1652569;
-float pib1 = 22.07, pib2 = 23.60;
+    char codigo1[4] = "A1", nome1[50] = "Santos";
+    char codigo2[4] = "B2", nome2[50] = "Londrina";
+    int populacao1 = 418608, populacao2 = 577318;
+    int pontos1 = 10, pontos2 = 80;
+    float area1 = 280674, area2 = 1652569;
+    float pib1 = 22.07, pib2 = 23.60;
+
+    // Cálculo da densidade populacional
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
+    // Cálculo do PIB per capita
+    float pib_per_capita1 = (pib1 * 1000000000) / populacao1;
+    float pib_per_capita2 = (pib2 * 1000000000) / populacao2;
