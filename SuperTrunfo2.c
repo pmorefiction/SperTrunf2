9b9c8564abc6725c8865df7a2903303acc3c998c
Samara Chaves <samarachaves1999@hotmail.com>
2025-06-08 19:29:22 -0300

    Create SuperTrunfo2.c
    Peguei os dados do trabalho passado

---
+++ SuperTrunfo2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+char codigo1[4] = "A1", nome1[50] = "Santos";
+char codigo2[4] = "B2", nome2[50] = "Londrina";
+int populacao1 = 418608, populacao2 = 577318;
+int pontos1 = 10, pontos2 = 80;
+float area1 = 280674, area2 = 1652569;
+float pib1 = 22.07, pib2 = 23.60;
